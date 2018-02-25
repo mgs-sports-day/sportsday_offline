efe613bd95aa3e71cf704ebdf8d0a38d5eedabb7
tti0 <theodoretucker.linuxdev@gmail.com>
2018-02-25 11:34:23 +0000

    Added manual data refresh backend.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// MGS Sports Day Results Viewer
+// index.js
+// https://github.com/tti0/sportsday
+
 function updateBoxes(res, com) {
   switch (res) {
     case 1:
@@ -30,6 +34,10 @@ function updateBoxes(res, com) {
 };
 
 function loadData() {
+  var url = 'https://docs.google.com/spreadsheets/d/1kApI3fZBqBqRx_egkx4BIGrWpoodAuyKd-e3lQ8CxMk';
+  var query = 'select A,B';
+  var queryEncoded = encodeURIComponent(query);
+  var finalURL = url + '/gviz/tq?tq=' + queryEncoded;
   var xhr = new XMLHttpRequest();
   xhr.open('GET', finalURL, true);
   xhr.send();
@@ -39,19 +47,16 @@ function loadData() {
       var responseComplete = xhr.responseText;
       var responseStarted = responseComplete.replace('/*O_o*/\ngoogle.visualization.Query.setResponse(','');
       var responseFinished = responseStarted.slice(0, -2);
-      console.log(responseFinished);
       var responseJSON = JSON.parse(responseFinished);
       statusResults = responseJSON.table.rows[0].c[1].v;
       statusCompets = responseJSON.table.rows[1].c[1].v;
-      console.log(statusResults, statusCompets);
       updateBoxes(statusResults, statusCompets);
     }
   }
 };
 
-var url = 'https://docs.google.com/spreadsheets/d/1kApI3fZBqBqRx_egkx4BIGrWpoodAuyKd-e3lQ8CxMk';
-var query = 'select A,B';
-var queryEncoded = encodeURIComponent(query);
-var finalURL = url + '/gviz/tq?tq=' + queryEncoded;
-console.log(finalURL);
+$( '#manual-refresh' ).click(function(){
+  loadData();
+});
+
 loadData();
